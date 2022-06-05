8f30189d005adc163328d5d856c1c2bad3ed8c4d
Shoney John <shoney.john@outlook.com>
2022-06-05 12:18:13 +0200

    feat(auth) : add JWT authetication to api

--- backend.js
+++ backend.js
@@ -1,10 +1,16 @@
 const express = require("express");
 
-const app =  express();
+const appUses = [
+    { apiPath: '/getroom', routerPath: './router/room' },
+    { apiPath: '/api/users', routerPath: './router/users' }
+]
+
+const app = express();
 const dbconf = require("./db")
 
-const room = require('./router/room')
-app.use('/getroom',room);
+appUses.forEach(use => {
+    app.use(use.apiPath, require(use.routerPath));
+})
 
 const port = process.env.port || 8888;
-app.listen (port,()=> console.log(`server started using port ${port}`));
+app.listen(port, () => console.log(`server started using port ${port}`));
