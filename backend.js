b585d828dc9c3e7887daf2b0fbdb85fa40689119
DESKTOP-FP4OP26\User <sivakotireddy234@gmail.com>
2022-06-09 19:08:33 +0200

    removing merge conflicts

--- backend.js
+++ backend.js
@@ -8,16 +8,12 @@ app.use(express.json())
 const room = require('./router/room')
 app.use('/getroom',room);
 
-const port = process.env.port || 8888;
-app.listen (port,()=> console.log(`server started using port ${port}`));
+
 
 const appUses = [
     { apiPath: '/getroom', routerPath: './router/room' },
     { apiPath: '/api/users', routerPath: './router/users' }
 ]
-
-const app = express();
-const dbconf = require("./db")
 
 appUses.forEach(use => {
     app.use(use.apiPath, require(use.routerPath));
--- router/room.js
+++ router/room.js
@@ -8,14 +8,10 @@ router.get("/getRooms",async(req,res)=> {
         res.send(getrooms);
 });
 
-<<<<<<< HEAD
 router.post("/getroombyid",async(req,res)=> {
 
         const roomid = req.body.roomid;
         const room = await Rooms.findOne({_id:roomid});
         res.send(room);
 });
-
-=======
->>>>>>> 2468531c5c9518bf4e8957c0b2033dc0c05648ca
 module.exports = router;
