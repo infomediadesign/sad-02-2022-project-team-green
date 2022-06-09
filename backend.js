af4d09b51a4eca07b1481dd11be135b05cd15f90
DESKTOP-FP4OP26\User <sivakotireddy234@gmail.com>
2022-06-09 19:00:11 +0200

    removing errors

--- backend.js
+++ backend.js
@@ -1,6 +1,5 @@
 const express = require("express");
 
-<<<<<<< HEAD
 const app =  express();
 const dbconf = require("./db")
 
@@ -11,7 +10,7 @@ app.use('/getroom',room);
 
 const port = process.env.port || 8888;
 app.listen (port,()=> console.log(`server started using port ${port}`));
-=======
+
 const appUses = [
     { apiPath: '/getroom', routerPath: './router/room' },
     { apiPath: '/api/users', routerPath: './router/users' }
@@ -26,4 +25,4 @@ appUses.forEach(use => {
 
 const port = process.env.port || 8888;
 app.listen(port, () => console.log(`server started using port ${port}`));
->>>>>>> 2468531c5c9518bf4e8957c0b2033dc0c05648ca
+
