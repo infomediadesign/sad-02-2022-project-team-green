0a842911d93212f95b672d8716d801dd84b37e0e
Shoney John <shoney.john@outlook.com>
2022-06-12 20:23:10 +0200

    Add signup

--- router/users.js
+++ router/users.js
@@ -1,12 +1,61 @@
-
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getMe } = require('../controllers/userController');
 
-const { protect } = require('../middleware/auth');
 
-router.post("/", registerUser);
-router.post("/login", loginUser);
-router.get("/", protect, getMe);
+const User = require("../model/users");
+
+router.post("/signup", async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        res.status(400)
+        throw new Error('Please add all fields');
+    }
+
+
+    // Check user exists
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    // Hash password
+
+    const salt = await bcrypt.genSalt(10);
+
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+
+    const user = await User.create({
+        username,
+        email,
+        password: hashPassword
+    });
+
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+});
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+}
+
 
 module.exports = router;
