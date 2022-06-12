c726a691a386e21a83585d1457614180e4fd4b8b
DESKTOP-FP4OP26\User <sivakotireddy234@gmail.com>
2022-06-12 11:22:41 +0200

    unit testing for ppost api

--- reservationtest.js
+++ reservationtest.js
@@ -24,4 +24,31 @@ describe('reservation',()=>{
         });
     });
     });
+    describe("post /reservation/reservenow",()=>{
+        it("all test cases passed",(done)=>{
+            const reserv ={
+                roomNumber:102,
+                roomid:"62a384f00e8c280b9ff49735",
+                userid:123,
+                checkin:"Jun 19th 2022, Sunday",
+                checkout:"Jun 21th 2022, Tuesday",
+                totalpayment:80,
+                totaldays:2,
+                status:"reserved"
+            };
+            chai.request('http://localhost:8888').post("/reservation/reservenow").send(reserv).end((err,response)=>{
+                response.should.have.status(200);
+                response.body.should.be.a('object');
+                response.body['reservations'].should.have.nested.property('roomNumber').eq(102);
+                response.body['reservations'].should.have.nested.property('roomid').eq("62a384f00e8c280b9ff49735");
+                response.body['reservations'].should.have.nested.property('userid').eq(123);
+                response.body['reservations'].should.have.nested.property('checkin').eq("Jun 19th 2022, Sunday");
+                response.body['reservations'].should.have.nested.property('checkout').eq("Jun 21th 2022, Tuesday");
+                response.body['reservations'].should.have.nested.property('totalpayment').eq(80);
+                response.body['reservations'].should.have.nested.property('totaldays').eq(2);
+                response.body['reservations'].should.have.nested.property('status').eq("reserved");               
+                done();
+            });
+        });
+    });
 });
