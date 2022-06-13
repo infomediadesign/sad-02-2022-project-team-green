1fb1545013b31218f403ef63cb48d8c4f2c4910c
DESKTOP-FP4OP26\User <sivakotireddy234@gmail.com>
2022-06-13 23:14:10 +0200

    styling register

--- reactcode/src/View/Signup.js
+++ reactcode/src/View/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Sweet from "sweetalert2"
-
+import hotelbackground from "../images/hotelbackground.png"
 function Signup(){
   
   const [username,saveUserName] = useState();
@@ -26,19 +26,18 @@ function Signup(){
   }, [])
   
   return (
-    <div className="container">
-    <div className="row justify-content-center">
-      <div className="col-md-5">
-            <div>
-              <h1>Signup</h1>
-              <input type="text" placeholder="User Name" value={username} onChange={(newuser)=>{saveUserName(newuser.target.value)}}></input><br/>
-              <input type="text" placeholder="Email" value={email} onChange={(newuser)=>{saveEmail(newuser.target.value)}}></input><br/>
-              <input type="text" placeholder="Password" value={password} onChange={(newuser)=>{savePassword(newuser.target.value)}}></input><br/>
-              <button className="btn btn-primary" onClick={signUp}>Signup</button><br/>
+    
+    <div className="row justify" style={{backgroundImage: `url(${hotelbackground})`,backgroundSize:'cover',width: '100vw',height: '100vh'}}>
+      <div className="container">
+            <div className="row justify-content-centre" style={{margin:'auto',width:'30%'}}>
+              <h1  style={{fontSize:'20px',color:'white',marginTop:'5%'}}>Signup</h1>
+              <input type="text" placeholder="User Name"  style={{marginTop:'2%'}} value={username} onChange={(newuser)=>{saveUserName(newuser.target.value)}}></input><br/>
+              <input type="text" placeholder="Email"  style={{marginTop:'2%'}} value={email} onChange={(newuser)=>{saveEmail(newuser.target.value)}}></input><br/>
+              <input type="text" placeholder="Password"  style={{marginTop:'2%'}} value={password} onChange={(newuser)=>{savePassword(newuser.target.value)}}></input><br/>
+              <button className="btn btn-primary"  style={{width:'45%',marginTop:'4%',marginLeft:"25%"}} onClick={signUp}>Signup</button><br/>
             </div>
+        </div>
       </div>
-    </div>
-    </div>
   );
   
 };
