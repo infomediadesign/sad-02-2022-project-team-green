23faa00eb080f64ef4ab13da5cf01af2d8b713ba
Ghavate <57428758+Ghavate@users.noreply.github.com>
2022-06-17 22:57:51 +0200

    Create server.js

---
+++ ChatFunctionality/utils/server.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'Greenbot ';
+
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room, email }) => {
+    const user = userJoin(socket.id, username, room, email);
+
+    socket.join(user.room);
+
+    socket.emit('message', formatMessage(botName, 'Welcome to Hotel Glory!'));
+
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+  });
+});
+
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
